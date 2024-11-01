35d0a4b3e3b9b35780160cadc6ed468296e0545c
PavaniMaram <pavanimaram15@gmail.com>
2024-11-01 23:54:35 +0530

    Modified Strings

--- src/main/java/com/learning/Strings/Strings.java
+++ src/main/java/com/learning/Strings/Strings.java
@@ -1,10 +1,53 @@
 package com.learning.Strings;
+import java.util.Scanner;
 
 public class Strings {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void lengthOfString() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String data = sc.nextLine();
+        
+        int length = data.length();
+        System.out.println("Length of the string: " + length);
+        
+        sc.close();
+    }
+        public static void countWords() {
+        	Scanner sc=new Scanner(System.in);
+        	System.out.println("Enter the String");
+        	String data=sc.nextLine();
+        	String arr[]=data.trim().split(" ");
+        	System.out.println(arr.length);
+        	
+        	sc.close();
+        	
+        }
+        public static void totalOccurrences() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the String");
+            String data = sc.nextLine();
+            char input[]=data.toCharArray();
+            int count[]=new int[256];
+            for(char c:input) {
+            	count[c]++;
+            }
+            for(int i=0;i<256;i++) {
+            	if(count[i]>0) {
+            		System.out.println((char)i+":"+ count[i]);
+            	}
+            }
+            
+            sc.close();
+        }
+            
+ 
+            
 
-	}
-
+        public static void main(String[] args) {
+            //lengthOfString();
+            //countWords();
+        	totalOccurrences();
+            
+        }
 }
