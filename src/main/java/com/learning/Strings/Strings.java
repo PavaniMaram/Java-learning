7901ea029c715741f558125991580c30e14c1b08
PavaniMaram <pavanimaram15@gmail.com>
2024-11-02 01:18:14 +0530

    Updated Strings

--- src/main/java/com/learning/Strings/Strings.java
+++ src/main/java/com/learning/Strings/Strings.java
@@ -27,19 +27,46 @@ public class Strings {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the String");
             String data = sc.nextLine();
+            System.out.println("Enter the target");
+            char target=sc.nextLine().charAt(0);
+         
             char input[]=data.toCharArray();
             int count[]=new int[256];
             for(char c:input) {
             	count[c]++;
             }
-            for(int i=0;i<256;i++) {
-            	if(count[i]>0) {
-            		System.out.println((char)i+":"+ count[i]);
-            	}
+         
+            	if(count[target]>0) {
+            		System.out.println((char)target+":"+ count[target]);
+            	
             }
             
             sc.close();
         }
+        public static void reverseString() {
+        	 Scanner sc = new Scanner(System.in);
+             System.out.println("Enter the String");
+             String data = sc.nextLine();
+             char arr[]=data.toCharArray();
+             for(int i=arr.length-1;i>=0;i--) {
+            	 System.out.print(arr[i]);
+             }
+             sc.close();
+        	
+        }
+        public static void reverseString2() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the String");
+            String data = sc.nextLine();
+            
+            // Use StringBuilder to reverse the string
+            StringBuilder sb = new StringBuilder(data);
+            sb.reverse(); // This method directly reverses the string
+
+            System.out.println("Reversed String: " + sb.toString());
+            sc.close();
+        }
+        
             
  
             
@@ -47,7 +74,9 @@ public class Strings {
         public static void main(String[] args) {
             //lengthOfString();
             //countWords();
-        	totalOccurrences();
+        	//totalOccurrences();
+        	//reverseString();
+        	reverseString2();
             
         }
 }
